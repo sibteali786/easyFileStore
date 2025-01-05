f5fa7a9d2aacc4e8ff349d67a01e420f05d3325d
Syed Sibteali Baqar <sibteali786@gmail.com>
2025-01-05 11:39:25 +0500

    feat: add user registration and password hashing functionality

--- easyFileStore_Cdk/lambda/go.mod
+++ easyFileStore_Cdk/lambda/go.mod
@@ -2,9 +2,10 @@ module lambda-func
 
 go 1.23.1
 
-require github.com/aws/aws-sdk-go v1.55.5
-
 require (
-	github.com/aws/aws-lambda-go v1.47.0 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/aws/aws-lambda-go v1.47.0
+	github.com/aws/aws-sdk-go v1.55.5
+	golang.org/x/crypto v0.31.0
 )
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- easyFileStore_Cdk/lambda/types/types.go
+++ easyFileStore_Cdk/lambda/types/types.go
@@ -1,6 +1,29 @@
 package types
 
+import "golang.org/x/crypto/bcrypt"
+
 type RegisterUser struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type User struct {
+	Username     string `json:"username"`
+	PasswordHash string `json:"password"`
+}
+
+func NewUser(registerUser RegisterUser) (User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(registerUser.Password), 10)
+	if err != nil {
+		return User{}, err
+	}
+	return User{
+		Username:     registerUser.Username,
+		PasswordHash: string(hashedPassword),
+	}, nil
+}
+
+func ValidatePassword(hasedPassword, plainTextPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainTextPassword))
+	return err == nil
 }
